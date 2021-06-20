428e9a85d8462fcb04869d01be00d727f2e60642
parallels <parallels@localhost.localdomain>
2021-06-20 22:14:32 +0800

    五子棋代码“
    git commit -m 五子棋代码“

---
+++ game/main.c
@@ -0,0 +1,24 @@
+#include "game.h"
+int main()
+{
+  int quit = 0;
+  int select = 0;
+  while(!quit)
+  {
+     Menu();
+     scanf("%d",&select);
+     switch(select)
+     {
+         case 1:
+	     Game();
+             break;
+	 case 0:
+             quit = 1;
+	     printf("ByeBye!\n");
+             break;
+	 default:
+	     printf("Enter Error,Try Again!\n");
+	     break;
+     }
+   }
+}
