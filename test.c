c451fd7c5c09781c0be610292217b449d4142aa7
parallels <parallels@localhost.localdomain>
2021-06-24 10:49:18 +0800

    乘法口诀表

--- test.c
+++ test.c
@@ -1,22 +1,24 @@
-
-//两个兵乓球队进行比赛，各出3人。甲队为A、B、C 3人，乙队为X、Y、Z 3人。已抽签决定比赛名单。
-//有人向队友打听比赛的名单。A说他不和X比，C说他不和X、Z比。请编程序找出3对赛手的名单。
-
 #include <stdio.h>
 int main()
-  {
-   char i,j,k;
-    for(i='X';i<='Z';i++)
-     for(j='X';j<='Z';j++)
-      if(i!=j)
-      for(k="X';k<='Z';k++)
-	    if(i!=k && j!=k)
-	     if(i!='X' && k!='X' && k!="Z")
-		printf("Avs%c\tBvs%c\tCvs%c\n",i,j,k);
-       
-   return 0; 
-  }
-
-
-
-
+{
+   int n = 0;
+   int arr[100] ={0};
+   scanf("%d",&n);
+   int i = 0;
+   for(i=1;i<=n;i++)
+   {
+     scanf("%d",&arr[i]);
+   }   
+   int x = 0;
+   scanf("%d",&x);
+   int count = 0;
+   for(i=0;i<n;i++)
+   {
+     if(x == arr[i])
+     {
+     count++;
+     }
+   }
+  printf("%d\n",count);   
+return 0;
+}
