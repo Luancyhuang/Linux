b81cfe3c4dfc5bddbd022b8aa8136c1ed62baa86
parallels <parallels@localhost.localdomain>
2021-07-21 12:54:12 +0800

    两个数组内容互换

--- 7_20_test.c
+++ 7_20_test.c
@@ -1,47 +1,21 @@
-//创建一个整型数组，完成对数组的操作
-//   1.实现函数Init()初始化数组为全0；
-//   2.实现Print()打印数组的每个元素；
-//   3.实现Reverse()函数实现数组的逆置。
-//要求：自己设计以上函数的参数，返回值。
-void Init(int arr[],int sz)
-{  
-  int i = 0 ;
-  for(i=0;i<sz;i++)
-  {
-    arr[i] = 0;
-  }        
-}
-void Print(int arr[],int sz)
-{
-  int i = 0;
-  for(i=0;i<sz;i++)
-  {
-    printf("%d ",arr[i]);
-  }
-  printf("\n");
-}
-
-void Reverse(int arr[],int sz)
-{
-  int left = 0;
-  int right = sz - 1;
-  while(left<right)
-  {
-    int tmp = arr[left];
-    arr[left] = arr[right];
-    arr[right] = tmp;
-    left++;
-    right--;
-  }
-}
+//将数组A中的内容和数组B中的内容交换
 #include <stdio.h>
 int main()
 {
-  int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-  int sz = sizeof(arr)/sizeof(arr[0]);
- // Init(arr,sz);
-  Print(arr,sz);
-  Reverse(arr,sz);
-  Print(arr,sz);
+  int arr1[] = {1,3,5,7,9};
+  int arr2[] = {2,4,6,8,10};
+  int sz = sizeof(arr1)/sizeof(arr1[0]);
+  int tmp = 0;
+  int i = 0;
+  for(i=0;i<sz;i++)
+  {
+    tmp = arr1[i];
+    arr1[i] = arr2[i];
+    arr2[i] = tmp;
+    printf("%d ",arr1[i]);
+  }
+  printf("\n");
+  for(i=0;i<sz;i++)
+    printf("%d ",arr2[i]);
   return 0;
 }
