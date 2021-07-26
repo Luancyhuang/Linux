ebef600f8703d83a4da72bd90968bbea11561f8a
parallels <parallels@localhost.localdomain>
2021-07-26 17:30:21 +0800

    函数递归实现n的阶乘

--- test1.c
+++ test1.c
@@ -1,34 +1,24 @@
-//青蛙跳台阶的问题：
-//一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。此时该青蛙跳上一个n级台阶总共有多少种跳法？
-//
-//算法分析：青蛙跳一级台阶有一种跳法，跳两级台阶有两种跳法，跳三级台阶有3种跳法，青蛙跳4个台阶有5种跳法，跳5个台阶有8种跳法...(类似斐波那契数列）；
-
 #include <stdio.h>
-int Fib(int n)
-{ 
-  if(n<=3)
-    return n;
-  else	  
-  {	
-    int a = 2;
-    int b = 3;
-    int c = 0;
-    while(n>3)
-    {
-      c = a+b;
-      a = b;
-      b = c;
-      n--;
-    }
-    return c;
+double Pow (int n,int k)
+{
+  if(k>0)
+  { 
+    return n*Pow(n,k-1);
+  }
+  else if(k==0)
+  {   
+    return 1.0;
+  }
+  else 
+  {  
+    return 1.0/Pow(n,-k); 
   }
 }
 int main()
 {
-  int n = 0;
-  printf("Please input your number:\n");
-  scanf("%d",&n);
-  int ret = Fib(n);
-  printf("%d\n",ret);
+  int n = 2;
+  int k = -3;
+  double ret = Pow(n ,k);
+  printf("%lf\n",ret);
   return 0;
 }
