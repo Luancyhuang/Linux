16c81bd8ee399534401e12e69f8eacb71bab767f
parallels <parallels@192.168.1.6>
2021-08-12 23:39:51 +0800

    sizeof 和strlen

--- test.c
+++ test.c
@@ -1,99 +1,63 @@
-//函数指针实现加减乘除计算器功能
-#include <stdio.h>
-int Add(int x,int y)
-{
-  return x+y;
-}
-int Sub(int x ,int y)
-{
-  return x-y;
-}
-int Mul(int x ,int y)
-{
-  return x*y;
-}
-int Div(int x ,int y)
-{
-  return x/y;
-}
-
-void menu()
-{
-  printf("***********************\n");
-  printf("**** 1.加法 2.减法 ****\n");
-  printf("**** 3.乘法 4.除法 ****\n");
-  printf("**** 0.exit        ****\n");
-  printf("***********************\n");
-}
+#include <stdio.h> 
+#include <string.h>
+//void show(const int *_p)
+//{
+//  printf("%p\n",&_p);
+//  printf("%d\n",*_p);
+//
+//}
+//int main()
+//{
+//  int a = 10;
+//  int *p = &a;
+//  printf("%p\n",&p);
+//  show(p);
 int main()
 {
- //  int (*pf1)(int,int) = Add();
- //  int (*pf2)(int,int) = Sub();
- //  int (*pf3)(int,int) = Mul();
- //  int (*pf4)(int,int) = Div();
- // int (*pfArr)[5](int,int) = {0,Add,Sub,Mul,Div};//pfArr就是一个函数指针的数组
- // int input = 0;	
- // do
- // {
- //  int x = 0;
- //  int y = 0;
- //  int ret = 0;
- //  menu();
- //  printf("请选择:>");
- //  scanf("%d",&input);
- //  printf("请输入两个操作数:>");
- //  scanf("%d %d",&x,&y);
- //  switch(input)
- //  {
- //    case 1:
- //       ret =  Add(x,y);
- //       printf("%d\n",ret);
- //       break;
- //    case 2:
- //       ret = Sub(x,y);
- //       printf("%d\n",ret);
- //       break;
- //    case 3:
- //       ret = Mul(x,y);
- //       printf("%d\n",ret);
- //       break;
- //    case 4:
- //       ret = Div(x,y);
- //       printf("%d\n",ret);
- //       break;
- //    case 0:
- //       printf("请退出计算器!\n");
- //       break;
- //    default:
- //       printf("选择错误，请重新输入!\n");
- //       break;
- //  }
- // }while(input);
-  int input = 0;
-  do
-  {
-    int x = 0;
-    int y = 0;
-    int ret = 0;
-    menu();
-    printf("请选择:>");
-    scanf("%d",&input);
-    int (*pfarr[5])(int,int)= {0,Add,Sub,Mul,Div};
-    if(input == 0)
-    {
-      printf("请退出计算器!\n");
-    } 
-    else if((input >=1) &&(input <= 4)) 
-    {
-      printf("请输入两个操作数:>");
-      scanf("%d %d",&x,&y);
-      ret = pfarr[input](x,y);
-      printf("ret = %d\n",ret);
-    } 
-    else
-    {
-      printf("选择错误，请重新输入！\n");
-    }
-  }while(input);
-  return 0;
+ // char arr[] = "abcdef";
+ // printf("%d",sizeof(arr));//sizeof(arr)计算的是数组的大小，单位是字节。
+ // printf("%d",sizeof(arr+0));//首元素的地址大小32位4字节，64位8字节。
+ // printf("%d",sizeof(*arr));//首元素的大小为1。
+ // printf("%d",sizeof(arr[1]));//第二个元素的大小1。
+ // printf("%d",sizeof(&arr));//取出的数组的地址，但也是地址，所以是4/8个字节
+ // printf("%d",sizeof(&arr+1));//跳过整个数组后的地址，取出的也是数组的地址 4/8
+ // printf("%d",sizeof(&arr[0]+1));//4/8 &arr[0]+1,取出的是第二个元素的地址，大小是4/8。	
+ // return 0;
+ // char arr[] = "abcdef";
+ // printf("%d\n",strlen(arr));//遇到‘\0'，就停下来。6
+ // printf("%d\n",strlen(arr+0));//6/首元素的地址。
+ // printf("%d\n",strlen(*arr));//err 97 a 非法访问内存strlen访问的是地址。
+ // printf("%d\n",strlen(arr[1]));//err 98 b 同上
+ // printf("%d\n",strlen(&arr));//取出的是数组的地址，存到数组指针里面char (*p)[7] = &arr; 6
+ // printf("%d\n",strlen(&arr+1)); //err 随机值
+ // printf("%d\n",strlen(&arr[0]+1));//5 
+  char *p = "abcdef";
+ // printf("%d\n",sizeof(p));//计算指针变量大小4/8
+ // printf("%d\n",sizeof(p+1));//p+1 找到的是字符b 的地址4/8
+ // printf("%d\n",sizeof(*p));//1（第一个字符-a）
+ // printf("%d\n",sizeof(p[0]));// 1int arr[10];arr[0] == *(arr+0); p[0] == *(p+0) == 'a'  
+ // printf("%d\n",sizeof(&p));//4/8 地址
+ // printf("%d\n",sizeof(&p+1));//4/8 地址
+ // printf("%d\n",sizeof(&p[0]+1));//4/8 地址
+  //printf("%d\n",strlen(p));// 6 存储的a的地址首元素地址
+ // printf("%d\n",strlen(p+1));// 5  存储的是b 的地址
+ // printf("%d\n",strlen(*p)); //err  字符 'a'
+ // printf("%d\n",strlen(p[0]));//err 同上  
+ // printf("%d\n",strlen(&p));//随机值  
+ // printf("%d\n",strlen(&p+1)); //随机值 
+ // printf("%d\n",strlen(&p[0]+1));//5  
+ // return 0;
+  int a[3][4] = {0};
+  printf("%d\n",sizeof(a));// 3*4*4=48
+  printf("%d\n",sizeof(a[0][0]));//4 第一个元素的大小 
+  printf("%d\n",sizeof(a[0]));//16 看成一维数组（第一行的数组名）4*4=16
+  printf("%d\n",sizeof(a[0]+1));//4/8 第一行首元素的地址+1 指向的是第一行第2个元素的地址
+  printf("%d\n",sizeof(*(a[0]+1)));//4/8
+  printf("%d\n",sizeof(a+1));//4/8第二行数组的地址
+  printf("%d\n",sizeof(*(a+1)));//16 计算第二行的大小
+  printf("%d\n",sizeof(&a[0]+1));// 4/8 第二行的地址
+  printf("%d\n",sizeof(*(&a[0]+1)));//16 
+  printf("%d\n",sizeof(*a));//16 第一行首元素地址第一行的大小 
+  printf("%d\n",sizeof(a[3]));//16 不会访问第四行sizeof不会计算第四行大小
+
 }
